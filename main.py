074ec5d927336a569e27180ce29ad3e0c1c88a2d
zdornov <maks000156@yandex.ru>
2020-02-28 16:04:03 +0400

    почти все готово

--- main.py
+++ main.py
@@ -1,25 +1,69 @@
 import  cv2
-import cv
-from skimage import img_as_float, img_as_ubyte
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread("rab.jpg",0)
-cv2.imshow('grey scale image',img)
 
-cv2.imshow("Image", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+def threshold_processing(img, threshold):
+    p = img.shape
+    width = p[1]
+    height = p[0]
 
-vals = img.flatten()
-plt.hist(vals, bins = range(256))
-plt.show()
+    for i in range(height):
+        for j in range(width):
+            if (img[i, j] > threshold):
+                img[i, j] = 255
+            else:
+                img[i, j] = 0
 
-ret,th1 = cv2.threshold(img,215,255,cv2.ADAPTIVE_THRESH_MEAN_C)
-# th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-#             cv2.THRESH_BINARY,11,2)
-# th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-#             cv2.THRESH_BINARY,11,2)
-cv2.imshow("Image", th1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return img
+
+
+def linear_contrast(img,minY,maxY):
+    p = img.shape
+    width = p[1]
+    height = p[0]
+    vals = img.flatten()
+    minX = min(vals)
+    maxX = max(vals)
+
+    for i in range(height):
+        for j in range(width):
+            img[i, j] = ((img[i, j] - minX) / (maxX - minX)) * (maxY - minY) + minY
+
+    cv2.imshow('Linear contrast', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def image_preparation(img, brightness_range):
+    p = img.shape
+    width = p[1]
+    height = p[0]
+    for i in range(height):
+        for j in range(width):
+            if ((img[i, j] > brightness_range[0]) and (img[i, j] < brightness_range[1])):
+                continue
+            else:
+                img[i, j] = 0
+    return img
+
+
+
+if __name__ == '__main__':
+
+    img = cv2.imread("rab.jpg",0)
+    cv2.imshow('Input image',img)
+
+    vals = img.flatten()
+    plt.hist(vals, bins = range(256))
+    print(min(vals))
+    plt.show()
+
+    cv2.imshow('Threshold processing', threshold_processing(img,195))
+
+    img = cv2.imread("rab.jpg",0)
+
+    cv2.imshow('Image preparation', image_preparation(img, [195,255]))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
