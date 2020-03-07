096dc7159be83ffc1e70201befc5fd52aca75caa
DonMins <maks000156@yandex.ru>
2020-03-07 20:34:35 +0400

    всю шикарно

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import  cv2
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -26,14 +25,14 @@ def linear_contrast(img,minY,maxY):
     vals = img.flatten()
     minX = min(vals)
     maxX = max(vals)
+    print("исхо", minX)
+    print("исх макс", maxX)
 
     for i in range(height):
         for j in range(width):
             img[i, j] = ((img[i, j] - minX) / (maxX - minX)) * (maxY - minY) + minY
 
-    cv2.imshow('Linear contrast', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    return img
 
 def image_preparation(img, brightness_range):
     p = img.shape
@@ -41,8 +40,8 @@ def image_preparation(img, brightness_range):
     height = p[0]
     for i in range(height):
         for j in range(width):
-            if ((img[i, j] > brightness_range[0]) and (img[i, j] < brightness_range[1])):
-                continue
+            if ((img[i, j] > brightness_range[0]) and (img[i, j] <= brightness_range[1])):
+                img[i, j] = 255
             else:
                 img[i, j] = 0
     return img
@@ -51,19 +50,26 @@ def image_preparation(img, brightness_range):
 
 if __name__ == '__main__':
 
-    img = cv2.imread("rab.jpg",0)
+    img = cv2.imread("rab2.jpg",0)
+    img2 = cv2.imread("girl.jpg",0)
     cv2.imshow('Input image',img)
 
     vals = img.flatten()
     plt.hist(vals, bins = range(256))
-    print(min(vals))
     plt.show()
 
     cv2.imshow('Threshold processing', threshold_processing(img,195))
 
-    img = cv2.imread("rab.jpg",0)
+    img = cv2.imread("rab2.jpg",0)
 
     cv2.imshow('Image preparation', image_preparation(img, [195,255]))
+
+    # cv2.imshow('linear_contrast', linear_contrast(img2,0,255))
+    # img3 = linear_contrast(img2, 0, 255)
+    # vals = img3.flatten()
+    # plt.hist(vals, bins = range(256))
+    # plt.show()
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
